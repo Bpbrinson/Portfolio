58339f57e78a83acf7b340f33bfbb905c4bed425
Bpbrinson <BrandonBrinson1@gmail.com>
2024-09-20 08:52:38 -0400

    Added routes to other sites

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/linux')
+def linux():
+    return render_template('linux.html')
+
+@app.route('/AWS')
+def AWS():
+    return render_template('AWS.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
