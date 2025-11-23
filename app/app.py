58d305c24f894e7efbe0a01c3a0e7c983e1146d6
Bpbrinson <120956416+Bpbrinson@users.noreply.github.com>
2025-11-23 16:15:57 -0500

    Change app port from 80 to 8000

--- app/app.py
+++ app/app.py
@@ -12,4 +12,4 @@ def create_app():
 # For local development: `python app.py`
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
